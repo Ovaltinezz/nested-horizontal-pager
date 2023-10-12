1307c605c9eacbab8b98c12ea7a3c89da13fb114
ovaltine <zhush_97@126.com>
2023-10-12 15:15:12 +0800

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "nested-horizontal-pager"
+include(":app")
+ 
